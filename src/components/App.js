d4399ce4b2089148ffe14fa3816b28ff9fd668a5
Nando <mnandohidayat@gmail.com>
2018-10-20 16:51:51 +0700

    import Inventory and Order

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,8 @@ class App extends React.Component {
         <div className="menu">
           <Header />
         </div>
-        {/* <Order /> */}
-        {/* <Inventory /> */}
+        <Order />
+        <Inventory />
       </div>
     );
   }
